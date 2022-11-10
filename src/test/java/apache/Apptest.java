da7a08d749e30b9f5e14f15de8a77d336812dab9
Prathamesh <prathamesh4402@gmail.com>
2022-11-10 20:26:30 +0530

    first commit

---
+++ src/test/java/apache/Apptest.java
@@ -0,0 +1,15 @@
+package apache;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello Maven",new String("Hello Maven"));
+	}
+
+}
+
